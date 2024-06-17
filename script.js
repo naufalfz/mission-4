7cb60efaad93df3242e87ea41bf4324a162bbcc4
ncode <78970289+naufalfz@users.noreply.github.com>
2024-06-17 17:22:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -97,11 +97,11 @@ btnSubmit.addEventListener('click', function (e) {
     Swal.fire("Data cannot be empty!!");
   } else {
     if (valPriority == 'High') {
-      valPriority = '<span class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-white">High</span>';
+      valPriority = '<span class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">High</span>';
     } else if (valPriority == 'Medium') {
-      valPriority = '<span class="bg-yellow-100 text-yellow-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-white">Medium</span>';
+      valPriority = '<span class="bg-yellow-100 text-yellow-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300">Medium</span>';
     } else {
-      valPriority = '<span class="bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-white">Low</span>';
+      valPriority = '<span class="bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">Low</span>';
     }
     
     todos.push({

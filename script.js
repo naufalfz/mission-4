75c50e14c63e2ee651c59dd3102c7eea8f6da44c
ncode <78970289+naufalfz@users.noreply.github.com>
2024-06-17 17:13:59 +0700

    Update script.js
    Text colour badges

--- script.js
+++ script.js
@@ -97,9 +97,9 @@ btnSubmit.addEventListener('click', function (e) {
     Swal.fire("Data cannot be empty!!");
   } else {
     if (valPriority == 'High') {
-      valPriority = '<span class="bg-red-100 text-white text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-white">High</span>';
+      valPriority = '<span class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-white">High</span>';
     } else if (valPriority == 'Medium') {
-      valPriority = '<span class="bg-yellow-100 text-white text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-white">Medium</span>';
+      valPriority = '<span class="bg-yellow-100 text-yellow-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-white">Medium</span>';
     } else {
       valPriority = '<span class="bg-green-100 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-white">Low</span>';
     }
